c9578151b3f3bc28cfa723de11c445cef9d6892f
nusratalam123 <nusratalam975@gmail.com>
2025-04-16 10:32:02 +0600

    add pic inside the app folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import Banner from "@/components/Banner"
-import SolutionSection from "@/components/SolutionSection"
-import ServicesSection from "@/components/ServicesSection"
-import InfoSection from "@/components/InfoSection"
-import MobileAppSection from "@/components/MobileAppSection"
+import Banner from "@/app/components/Banner"
+import SolutionSection from "@/app/components/SolutionSection"
+import ServicesSection from "@/app/components/ServicesSection"
+import InfoSection from "@/app/components/InfoSection"
+import MobileAppSection from "@/app/components/MobileAppSection"
 
 export default function Home() {
   return (
