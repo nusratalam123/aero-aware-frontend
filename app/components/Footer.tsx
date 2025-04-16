0e2b90bcdbd5c6a7468423c86d582324da0933d4
nusratalam123 <nusratalam975@gmail.com>
2025-04-16 10:23:29 +0600

    change position of the layout,banner,footer,info section,,it is given the info section

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+import { Button } from "@/app/components/ui/button"
+import { Input } from "@/app/components/ui/input"
 
 export default function Footer() {
   return (
